8cb1a197b077d32b3565665e1e81b5dfa7878143
Wills Liou <willsliou@protonmail.com>
2020-03-28 08:22:34 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+from app_class import *
+
+if __name__ == "__main__":  # if run main, run app
+    app = App()
+    app.run()
+
---
+++ settings.py
@@ -0,0 +1,8 @@
+WIDTH = 800
+HEIGHT = 600
+
+
+
+WHITE = (255,255,255)
+
+testBoard = [[0 for x in range(9)] for x in range(9)]
